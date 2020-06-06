82049b84b972599cc463a6b86b63fb341bc96a6e
Revolving_DCON <bitbucket@danielconlon.com>
2020-06-06 04:47:07 +0100

    Added massive templating changes, and some states.

---
+++ index.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const path = require("path");
+const fs = require('fs');
+const copydir = require('copy-dir');
+const del = require('del');
+const glob = require('glob');
+const app = express();
+const ejs = require('ejs');
+const moment = require('moment');
+const cron = require("node-cron");
+// const kv = require('./local_modules/cloudflare-workers-kv');
+const chokidar = require('chokidar');
+const fetch = require('node-fetch');
+const async = require('async');
+const col = require('chalk');
+const figlet = require('figlet');
+var protests = require('./protests.js').protests;
+// const env = require('./env.json'); // Don't edit this without talking to DCON it could break things in production
+
+protests.pages = {
+	locations:{
+	    home:{display_name:'Home',path:'',template:'index.ejs'},
+	    safety:{display_name:'Safety',path:'safety',template:'safety.ejs'},
+	    petitions:{display_name:'Petitions',path:'petitions',template:'petitions.ejs'},
+	    education:{display_name:'Education',path:'education',template:'education.ejs'},
+	    team:{display_name:'Team',path:'team',template:'team.ejs'},
+	}
+};
+
+var args = process.argv.splice(2);
+var task = args[0];
+
+var build_pages = function() {
+	return new Promise(async (resolve,reject) => {
+		console.log(col.red('Deleting: dist/**'));
+	    const deletedPaths = await del(['dist']); // DELETE EVERYTHING INSIDE OF THE DIST FOLDER DON'T PLAY AROUND WITH THIS IT CAN WIPE YOUR OS!!
+
+	    if (!fs.existsSync('dist/')) {
+	        fs.mkdirSync('dist');
+	        fs.mkdirSync('dist/assets');
+	    }
+
+	    var files = glob.sync('src/*');
+	    files.forEach((v) => {
+	    	var finalDir = v.split('/');
+	    	finalDir.shift();
+	    	finalDir = finalDir.join('/');
+
+	    	var dirs = finalDir.split('/');
+			var dirstr = '';
+
+			for(var i in dirs){
+				dirstr += dirs[i]+'/';
+				if(!fs.existsSync('dist/assets/'+dirstr)){
+				    fs.mkdirSync('dist/assets/'+dirstr);
+				}
+			}
+
+			console.log(col.green(`Copying`),col.yellowBright(v));
+	    	copydir.sync(v,'dist/assets/'+finalDir);
+	    });
+
+	    for(var i in protests){
+	    	var region = protests[i];
+
+	    	for(var o in region.locations){
+	    		var loc = region.locations[o];
+
+	    		var path = (loc.path != null?loc.path:region.country_code+'/'+o)
+
+		        var dirs = path.split('/');
+				var dirstr = '';
+
+				for(var i in dirs){
+					dirstr += dirs[i]+'/';
+					if(dirstr && !fs.existsSync('dist/'+dirstr)){
+					    fs.mkdirSync('dist/'+dirstr);
+					}
+				}
+
+				((path,loc)=>{
+					console.log(col.green("Building page:"),col.yellowBright('/'+path));
+				    var str = ejs.renderFile('./src/templates/'+loc.template,{
+				    	data: {
+					        ...loc
+					    }
+				    },function(err,str){
+				    	if(err){
+				    		error = err;
+				    		console.log(col.bgRed('Fatal Error'));
+				    		console.log(col.red(err));
+				    	}
+				    	fs.writeFileSync('dist/'+path+'/index.html',str);
+				    });
+				})(path,loc);
+	    	};
+	    };
+
+		// var error = '';
+		// protests.forEach(async (v, i) => {
+		// 	if(error)return;
+
+		//     console.log(col.green("Building page:"),col.yellowBright('/'+v.path));
+		//     var str = ejs.renderFile(v.data,{
+		//     	data: {
+		// 	    	...protests,
+		// 	        page:v
+		// 	    }
+		//     },function(err,str){
+		//     	if(err){
+		//     		error = err;
+		//     		console.log(col.bgRed('Fatal Error'));
+		//     		console.log(col.red(err));
+		//     	}
+		//     	fs.writeFileSync('dist/'+v.path+'/index.html',str);
+		//     });
+		// });
+
+		resolve();
+	});
+};
+
+build_pages();
+const watcher = chokidar.watch(glob.sync('{src}/**')).on('change',path => {
+	build_pages();
+});
+
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+app.listen(process.env.PORT || 3000);
+console.log(col.bgCyan("Listening on port: " + 3000));
+console.log("Ready for changes");
+
---
+++ protests.js
@@ -0,0 +1,98 @@
+exports.protests = {
+	unitedstates:{
+		display_name:'United States',
+		country_code:'us',
+		locations:{
+			alabama:{
+				display_name:'Alabama',
+				legacy:'Alabama.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/alabama.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			alaska:{
+				display_name:'Alaska',
+				legacy:'Alaska.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/alaska.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			arizona:{
+				display_name:'Arizona',
+				legacy:'Arizona.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/arizona.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			arkansas:{
+				display_name:'Arkansas',
+				legacy:'Arkansas.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/arkansas.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			california:{
+				display_name:'California',
+				legacy:'California.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/california.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			colorado:{
+				display_name:'Colorado',
+				legacy:'Colorado.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/colorado.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			connecticut:{
+				display_name:'Connecticut',
+				legacy:'Connecticut.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/connecticut.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			delaware:{
+				display_name:'Delaware',
+				legacy:'Delaware.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/delaware.ejs',
+				parts:[
+					{
+
+					}
+				]
+			},
+			florida:{
+				display_name:'Florida',
+				legacy:'Florida.html', // Supporting avis shit initial code /sigh
+				template:'unitedstates/states/florida.ejs',
+				parts:[
+					{
+
+					}
+				]
+			}
+		}
+	}
+};
